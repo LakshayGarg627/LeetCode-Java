be85c3be4a83c9f34db85de6600b216025d4d50c
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-15 19:48:18 +0530

    Time: 2 ms (24.06%), Space: 45.2 MB (17.19%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -3,12 +3,13 @@ class Solution {
         int n=chars.length;
         StringBuilder sb=new StringBuilder("");
         for(int i=0;i<n;i++){
+            sb.append(chars[i]);
             int count=1;
             while(i<n-1 && chars[i]==chars[i+1]){
                 count++;
                 i++;
             }
-            sb.append(chars[i]);
+            
             if(count>1){
                 sb.append(count);
             }
