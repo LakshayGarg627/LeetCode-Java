c86a8c81d3d90461c7c94d8c5bd9100d6d3a2ead
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-15 20:12:41 +0530

    Time: 1 ms (99.8%), Space: 43.1 MB (69.59%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -2,23 +2,36 @@ class Solution {
     public String removeOccurrences(String s, String part) {
         StringBuilder sb=new StringBuilder(s);
         int n=part.length();
-        while(sb.length()>n){
-            boolean removed=false;
-            for(int i=0;i<sb.length()-n+1;i++){
-                String substr=sb.substring(i,i+n);
-                if(substr.equals(part)){
-                    sb.delete(i,i+n);
-                    removed=true;
-                    break;
-                }
-            }
-            if(!removed){
-                break;
-            }
-        }
-        if(sb.toString().equals(part)){
-            return "";
+        int idx=sb.indexOf(part);
+        while(idx!=-1){
+            sb.delete(idx,idx+n);
+            idx=sb.indexOf(part);
         }
         return sb.toString();
     }
 }
+
+// class Solution {
+//     public String removeOccurrences(String s, String part) {
+//         StringBuilder sb=new StringBuilder(s);
+//         int n=part.length();
+//         while(sb.length()>n){
+//             boolean removed=false;
+//             for(int i=0;i<sb.length()-n+1;i++){
+//                 String substr=sb.substring(i,i+n);
+//                 if(substr.equals(part)){
+//                     sb.delete(i,i+n);
+//                     removed=true;
+//                     break;
+//                 }
+//             }
+//             if(!removed){
+//                 break;
+//             }
+//         }
+//         if(sb.toString().equals(part)){
+//             return "";
+//         }
+//         return sb.toString();
+//     }
+// }
