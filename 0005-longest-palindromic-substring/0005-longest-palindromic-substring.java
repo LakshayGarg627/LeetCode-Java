fa19f8ae96b1b5cc9f48c19eafb03fc00b102d01
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-17 13:29:03 +0530

    Time: 19 ms (45.83%), Space: 46.6 MB (14.84%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,66 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans="";
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int left=i;
+            int right=i;
+
+            while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
+                left--;
+                right++;
+            }
+            
+
+            String palindrome=s.substring(left+1,right);
+            if(palindrome.length()>ans.length()){
+                ans=palindrome;
+            }
+
+            left=i;
+            right=i+1;
+             while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
+                left--;
+                right++;
+            }
+            
+            palindrome=s.substring(left+1,right);
+            if(palindrome.length()>ans.length()){
+                ans=palindrome;
+            }
+        }
+        return ans;
+    }
+}
+
+// BRUTE FORCE
+
+// class Solution {
+//     public boolean palindrome(String s){
+//         int i=0;
+//         while(i<s.length()/2){
+//             if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+//                 return false;
+//             }
+//             i++;
+//         }
+//         return true;
+//     }
+//     public String longestPalindrome(String s) {
+//         int size=0;
+//         String ans="";
+//         for(int i=0;i<s.length();i++){
+//             for(int j=i;j<s.length();j++){
+//                if(palindrome(s.substring(i,j+1))){
+//                     if(size<j-i+1){
+//                         size=j-i+1;
+//                         ans=s.substring(i,j+1);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+
+//     }
+// }
