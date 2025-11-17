9ee9891fe7e3784eb17ef4c9bd4ee07ad68e60bb
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-17 11:13:48 +0530

    Time: 1 ms (99.71%), Space: 65.6 MB (5.71%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,18 +1,34 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        List<Integer> list=new ArrayList<>();
         int n=nums.length;
+        int j=-1;
         for(int i=0;i<n;i++){
             if(nums[i]==1){
-                list.add(i);
-            }
-        }
-        for(int i=0;i<list.size()-1;i++){
-            if((list.get(i+1)-list.get(i)-1)<k){
-                return false;
+                if(j!=-1 && (i-j-1)<k){
+                    return false;
+                }
+                j=i;
             }
         }
         return true;
-    
     }
 }
+
+// class Solution {
+//     public boolean kLengthApart(int[] nums, int k) {
+//         List<Integer> list=new ArrayList<>();
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==1){
+//                 list.add(i);
+//             }
+//         }
+//         for(int i=0;i<list.size()-1;i++){
+//             if((list.get(i+1)-list.get(i)-1)<k){
+//                 return false;
+//             }
+//         }
+//         return true;
+    
+//     }
+// }
