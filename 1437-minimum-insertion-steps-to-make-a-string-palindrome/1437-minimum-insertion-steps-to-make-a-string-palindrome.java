bc3c756600b073f250fa8e78545c669374c617ed
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-17 22:58:54 +0530

    Time: 27 ms (40.42%), Space: 47.7 MB (8.75%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -16,13 +16,28 @@ class Solution {
         }
         return dp[n][m];
     }
+    public int tab(String s1,String s2){
+        int n=s1.length();
+        int dp[][]=new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
     public int minInsertions(String s) {
         int n=s.length();
-        int dp[][]=new int[n+1][n+1];
-        for(int i=0;i<n+1;i++){
-            Arrays.fill(dp[i],-1);
-        }
+        // int dp[][]=new int[n+1][n+1];
+        // for(int i=0;i<n+1;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
         String temp=new StringBuilder(s).reverse().toString();
-        return s.length()-lps(s,temp,n-1,n-1,dp);
+        // return s.length()-lps(s,temp,n-1,n-1,dp);
+        return n-tab(s,temp);
     }
 }
