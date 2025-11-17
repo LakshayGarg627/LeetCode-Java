be826c61a64866fef4ba11408002527a0f807202
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-17 11:12:18 +0530

    Time: 4 ms (13.43%), Space: 67.7 MB (5.71%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -7,7 +7,6 @@ class Solution {
                 list.add(i);
             }
         }
-        System.out.println(list);
         for(int i=0;i<list.size()-1;i++){
             if((list.get(i+1)-list.get(i)-1)<k){
                 return false;
