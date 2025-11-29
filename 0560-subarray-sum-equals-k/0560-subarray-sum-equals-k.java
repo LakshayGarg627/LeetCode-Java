3ac31482a2ba1720dd73d1516f78dd6cdca67b8c
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-11-30 01:58:06 +0530

    Time: 24 ms (74.23%), Space: 49.1 MB (8.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,26 +1,45 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n=nums.length;
+        int sum=0;
+        int ans=0;
+
         HashMap<Integer,Integer> map=new HashMap<>();
-        int prefix[]=new int[n];
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        } 
-        int count=0;
+        map.put(0,1);
         for(int j=0;j<n;j++){
-            if(prefix[j]==k){
-                count++;
+            sum+=nums[j];
+            if(map.containsKey(sum-k)){
+                ans+=map.get(sum-k);
             }
-            int value=prefix[j]-k;
-            if(map.containsKey(value)){
-                count+=map.get(value);
-            }
-            map.put(prefix[j],map.getOrDefault(prefix[j],0)+1);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        return count;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int prefix[]=new int[n];
+//         prefix[0]=nums[0];
+//         for(int i=1;i<n;i++){
+//             prefix[i]=prefix[i-1]+nums[i];
+//         } 
+//         int count=0;
+//         for(int j=0;j<n;j++){
+//             if(prefix[j]==k){
+//                 count++;
+//             }
+//             int value=prefix[j]-k;
+//             if(map.containsKey(value)){
+//                 count+=map.get(value);
+//             }
+//             map.put(prefix[j],map.getOrDefault(prefix[j],0)+1);
+//         }
+//         return count;
+//     }
+// }
 
 // Optimized code
 // class Solution {
