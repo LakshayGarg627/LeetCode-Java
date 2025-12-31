520fc28d0cd20a4d9a309127a69a7fb17a2fa916
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2025-12-31 22:46:30 +0530

    Time: 3 ms (64.09%), Space: 46.3 MB (65.7%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,35 +14,69 @@
  * }
  */
 class Solution {
-    
     public int findBottomLeftValue(TreeNode root) {
-        if(root==null){
-            return -1;
-        }
-        Queue<TreeNode> q=new LinkedList<>();
+        Queue<TreeNode>q=new LinkedList<>();
         q.add(root);
-        q.add(null);
-        List<List<Integer>> result=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
+        int ans=root.val;
         while(!q.isEmpty()){
-            TreeNode curr=q.poll();
-            if(curr==null){
-                result.add(temp);
-                temp=new ArrayList<>();
-                if(!q.isEmpty()){
-                    q.add(null);
-                }
-            }else{
-                temp.add(curr.val);
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right!=null){
-                    q.add(curr.right);
-                }
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                if(i==0)ans=curr.val;
+                if(curr.left!=null)q.add(curr.left);
+                if(curr.right!=null)q.add(curr.right);
             }
         }
-        List<Integer> arr=result.get(result.size()-1);
-        return arr.get(0);
+        return ans;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+    
+//     public int findBottomLeftValue(TreeNode root) {
+//         if(root==null){
+//             return -1;
+//         }
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.add(root);
+//         q.add(null);
+//         List<List<Integer>> result=new ArrayList<>();
+//         List<Integer> temp=new ArrayList<>();
+//         while(!q.isEmpty()){
+//             TreeNode curr=q.poll();
+//             if(curr==null){
+//                 result.add(temp);
+//                 temp=new ArrayList<>();
+//                 if(!q.isEmpty()){
+//                     q.add(null);
+//                 }
+//             }else{
+//                 temp.add(curr.val);
+//                 if(curr.left!=null){
+//                     q.add(curr.left);
+//                 }
+//                 if(curr.right!=null){
+//                     q.add(curr.right);
+//                 }
+//             }
+//         }
+//         List<Integer> arr=result.get(result.size()-1);
+//         return arr.get(0);
+//     }
+// }
