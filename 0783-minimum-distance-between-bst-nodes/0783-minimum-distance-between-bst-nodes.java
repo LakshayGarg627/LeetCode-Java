e96945f677cdf8f9ee3c14e88799f6a0af662450
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-02-02 01:46:30 +0530

    Time: 0 ms (100%), Space: 43.2 MB (23.84%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -14,24 +14,62 @@
  * }
  */
 class Solution {
-    Integer prev=null;
+    TreeNode prev=null;
     int min=Integer.MAX_VALUE;
-    public void inorder(TreeNode root){
+    public void helper(TreeNode root){
         if(root==null){
             return;
-
         }
-        inorder(root.left);
+        helper(root.left);
         if(prev!=null){
-            min=Math.min(min,root.val-prev);
+            min=Math.min(min,root.val-prev.val);
         }
-        prev=root.val;
-        inorder(root.right);
-    }
+        prev=root;
+        helper(root.right);
+    } 
     public int minDiffInBST(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
         min=Integer.MAX_VALUE;
-        prev=null;
-        inorder(root);
+        helper(root);
         return min;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     Integer prev=null;
+//     int min=Integer.MAX_VALUE;
+//     public void inorder(TreeNode root){
+//         if(root==null){
+//             return;
+//         }
+//         inorder(root.left);
+//         if(prev!=null){
+//             min=Math.min(min,root.val-prev);
+//         }
+//         prev=root.val;
+//         inorder(root.right);
+//     }
+//     public int minDiffInBST(TreeNode root) {
+//         min=Integer.MAX_VALUE;
+//         prev=null;
+//         inorder(root);
+//         return min;
+//     }
+// }
