5eaebafeed9cada12390a1eb488f185bcc0b0389
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-02-09 16:32:51 +0530

    Time: 8 ms (60.27%), Space: 46.9 MB (23.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,29 @@
 class Solution {
-    public int helper(int nums[],int target,int n,int idx,int result){
+    static int offset;
+    public int helper(int nums[],int target,int n,int idx,int result,int dp[][]){
         if(idx==n){
             return result==target?1:0;
         }
-        int way1=helper(nums,target,n,idx+1,result+nums[idx]);
-        int way2=helper(nums,target,n,idx+1,result-nums[idx]);
+        if(dp[idx][result+offset]!=-1){
+            return dp[idx][result+offset];
+        }
+        int way1=helper(nums,target,n,idx+1,result+nums[idx],dp);
+        int way2=helper(nums,target,n,idx+1,result-nums[idx],dp);
 
-        return way1+way2;
+        dp[idx][result+offset] = way1+way2;
+        return dp[idx][result+offset];
     }
     public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
         int n=nums.length;
-        return helper(nums,target,n,0,0);
+        int dp[][]=new int[n+1][2*sum+1];
+        for(int i=0;i<n+1;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        offset=sum;
+        return helper(nums,target,n,0,0,dp);
     }
 }
