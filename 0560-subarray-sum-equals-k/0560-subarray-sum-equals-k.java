91f79907f7156d6daa8f57e899333346ff73b9fc
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-14 16:31:37 +0530

    Time: 23 ms (93.91%), Space: 48.7 MB (68.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,21 +1,39 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n=nums.length;
-        int sum=0;
-        int ans=0;
         HashMap<Integer,Integer> map=new HashMap<>();
         map.put(0,1);
-        for(int j=0;j<n;j++){
-            sum+=nums[j];
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
             if(map.containsKey(sum-k)){
-                ans+=map.get(sum-k);
+                ans=ans+map.get(sum-k);
             }
             map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return ans;
+
     }
 }
 
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         map.put(0,1);
+//         int sum=0;
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//             if(map.containsKey(sum-k)){
+//                 ans=ans+map.get(sum-k);
+//             }
+//             map.put(sum,map.getOrDefault(sum,0)+1);
+//         }
+//         return ans;
+//     }
+// }
 
 // class Solution {
 //     public int subarraySum(int[] nums, int k) {
@@ -54,48 +72,6 @@ class Solution {
 //                 count+=map.get(value);
 //             }
 //             map.put(prefix[j],map.getOrDefault(prefix[j],0)+1);
-//         }
-//         return count;
-//     }
-// }
-
-// Optimized code
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int n=nums.length;
-//         HashMap<Integer,Integer> map=new HashMap<>();
-//         int prefix[]=new int[n];
-//         prefix[0]=nums[0];
-//         for(int i=1;i<n;i++){
-//             prefix[i]=prefix[i-1]+nums[i];
-//         }
-//         int count=0;
-//         for(int j=0;j<n;j++){
-//             if(prefix[j]==k){
-//                 count++;
-//             }
-//             int value=prefix[j]-k;
-//             if(map.containsKey(value)){
-//                 count+=(map.get(value));
-//             }
-//             map.put(prefix[j],map.getOrDefault(prefix[j],0)+1);
-//         }
-//         return count;
-//     }
-// }
-
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int count=0;
-//         int n=nums.length;
-//         for(int i=0;i<n;i++){
-//             int sum=0;
-//             for(int j=i;j<n;j++){
-//                 sum+=nums[j];
-//                 if(sum==k){
-//                     count++;
-//                 }
-//             }
 //         }
 //         return count;
 //     }
