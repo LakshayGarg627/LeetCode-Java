6136a00315da6b6bb91856715742a5cf9ea41f1e
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-20 18:17:13 +0530

    Time: 1 ms (99.15%), Space: 43 MB (10.05%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int size(long n){
+        int count=0;
+        while(n>0){
+            int rem=(int)n%10;
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+    public long countCommas(long n) {
+        if(n<1000){
+            return 0;
+        }
+        long count=0;
+        int len=size(n);
+        if(len==4 || len==5 || len==6){
+            count=count+(n-1000+1);
+        }else if(len==7 || len==8 || len==9){
+            count=count+(n-1000+1)+(n-1000000+1);
+        }
+        else if(len==10 || len==11 || len==12){
+            count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000+1);
+        }else if(len==13 || len==14 || len==15){
+            count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1);
+        }else{
+            count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1)+(n-1000000000000000L+1);
+        }
+        return count;
+    }
+}
