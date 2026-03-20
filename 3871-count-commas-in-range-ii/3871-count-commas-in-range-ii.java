4f7391c2e9208edefa56f20ff5a652b5250de5f3
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-20 18:25:27 +0530

    Time: 1 ms (99.15%), Space: 42.8 MB (48.48%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,29 +1,19 @@
 class Solution {
-    public int size(long n){
-        int count=0;
-        while(n>0){
-            int rem=(int)n%10;
-            count++;
-            n=n/10;
-        }
-        return count;
-    }
     public long countCommas(long n) {
         if(n<1000){
             return 0;
         }
-        String str=Long.toString(n);
+        
         long count=0;
-        // int len=size(n);
-        int len=str.length();
-        if(len==4 || len==5 || len==6){
+        
+        if(n>=1000 && n<=999999){
             count=count+(n-1000+1);
-        }else if(len==7 || len==8 || len==9){
+        }else if(n>=1000000 && n<=999999999L){
             count=count+(n-1000+1)+(n-1000000+1);
         }
-        else if(len==10 || len==11 || len==12){
+        else if(n>=1000000000L && n<=999999999999L){
             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000+1);
-        }else if(len==13 || len==14 || len==15){
+        }else if(n>=1000000000000L && n<999999999999999L){
             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1);
         }else{
             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1)+(n-1000000000000000L+1);
@@ -31,3 +21,37 @@ class Solution {
         return count;
     }
 }
+
+// class Solution {
+//     public int size(long n){
+//         int count=0;
+//         while(n>0){
+//             int rem=(int)n%10;
+//             count++;
+//             n=n/10;
+//         }
+//         return count;
+//     }
+//     public long countCommas(long n) {
+//         if(n<1000){
+//             return 0;
+//         }
+//         String str=Long.toString(n);
+//         long count=0;
+//         // int len=size(n);
+//         int len=str.length();
+//         if(len==4 || len==5 || len==6){
+//             count=count+(n-1000+1);
+//         }else if(len==7 || len==8 || len==9){
+//             count=count+(n-1000+1)+(n-1000000+1);
+//         }
+//         else if(len==10 || len==11 || len==12){
+//             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000+1);
+//         }else if(len==13 || len==14 || len==15){
+//             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1);
+//         }else{
+//             count=count+(n-1000+1)+(n-1000000+1)+(n-1000000000L+1)+(n-1000000000000L+1)+(n-1000000000000000L+1);
+//         }
+//         return count;
+//     }
+// }
