712f1061345f3e7dcc8b690ae920997dfd8c8a75
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-22 14:18:07 +0530

    Time: 0 ms (100%), Space: 43.8 MB (32.39%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -36,27 +36,77 @@ class Solution {
         if(mat.length!=target.length || mat[0].length != target[0].length){
             return false;
         }
-        int n=mat.length;
-        int m=mat[0].length;
-
-        int copy[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                copy[i][j]=mat[i][j];
-            }
-        }
-        boolean result=false;
-        while(true){
+        int i=4;
+        while(i>0){
             if(isEqual(mat,target)){
-                result=true;
-                break;
+                return true;
             }
             rotate(mat);
-            if(isEqual(mat,copy)){
-                result=false;
-                break;
-            }
+            i--;
         }
-        return result;
+        return false;
     }
 }
+
+// class Solution {
+//     public boolean isEqual(int mat1[][],int mat2[][]){
+//         int n=mat1.length;
+//         int m=mat1[0].length;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(mat1[i][j]!=mat2[i][j]){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     public void rotate(int mat[][]){
+//         int n=mat.length;
+//         int m=mat[0].length;
+//         for(int i=0;i<n/2;i++){
+//             int temp[]=mat[i];
+//             mat[i]=mat[n-i-1];
+//             mat[n-i-1]=temp;          
+//         }
+//         int trans[][]=new int[m][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 trans[i][j]=mat[j][i];
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 mat[i][j]=trans[i][j];
+//             }
+//         }
+
+//     }
+//     public boolean findRotation(int[][] mat, int[][] target) {
+//         if(mat.length!=target.length || mat[0].length != target[0].length){
+//             return false;
+//         }
+//         int n=mat.length;
+//         int m=mat[0].length;
+
+//         int copy[][]=new int[n][m];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 copy[i][j]=mat[i][j];
+//             }
+//         }
+//         boolean result=false;
+//         while(true){
+//             if(isEqual(mat,target)){
+//                 result=true;
+//                 break;
+//             }
+//             rotate(mat);
+//             if(isEqual(mat,copy)){
+//                 result=false;
+//                 break;
+//             }
+//         }
+//         return result;
+//     }
+// }
