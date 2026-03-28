3a0ef9a520ac618dfccff8ad7844416f8d0ae1ba
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-28 22:31:29 +0530

    Time: 0 ms (100%), Space: 43.2 MB (99.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,62 @@
 class Solution {
-    public int find(int arr[],int start,int end,int target){
+    public int helper(int arr[],int start,int end,int target){
         if(start>end){
             return -1;
         }
         int mid=start+(end-start)/2;
-
         if(arr[mid]==target){
             return mid;
         }
-
-        // mid on L1
         if(arr[start]<=arr[mid]){
             if(arr[start]<=target && target<=arr[mid]){
-                return find(arr,start,mid,target);
+                return helper(arr,start,mid,target);
             }else{
-                return find(arr,mid+1,end,target);
+                return helper(arr,mid+1,end,target);
             }
-        }
-        // mid on L2
-        else{
+        }else{
             if(arr[mid]<=target && target<=arr[end]){
-                return find(arr,mid+1,end,target);
+                return helper(arr,mid+1,end,target);
             }else{
-                return find(arr,start,mid,target);
+                return helper(arr,start,mid,target);
             }
         }
     }
     public int search(int[] nums, int target) {
         int n=nums.length;
-        return find(nums,0,n-1,target);
+        return helper(nums,0,n-1,target);
     }
 }
+
+// class Solution {
+//     public int find(int arr[],int start,int end,int target){
+//         if(start>end){
+//             return -1;
+//         }
+//         int mid=start+(end-start)/2;
+
+//         if(arr[mid]==target){
+//             return mid;
+//         }
+
+//         // mid on L1
+//         if(arr[start]<=arr[mid]){
+//             if(arr[start]<=target && target<=arr[mid]){
+//                 return find(arr,start,mid,target);
+//             }else{
+//                 return find(arr,mid+1,end,target);
+//             }
+//         }
+//         // mid on L2
+//         else{
+//             if(arr[mid]<=target && target<=arr[end]){
+//                 return find(arr,mid+1,end,target);
+//             }else{
+//                 return find(arr,start,mid,target);
+//             }
+//         }
+//     }
+//     public int search(int[] nums, int target) {
+//         int n=nums.length;
+//         return find(nums,0,n-1,target);
+//     }
+// }
