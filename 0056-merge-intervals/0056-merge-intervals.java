2564397b0b51b128d8a2d073cd3e6b8e947e14c2
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-30 17:31:07 +0530

    Time: 9 ms (37.19%), Space: 49.1 MB (65.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,98 +2,78 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         int n=intervals.length;
         int m=intervals[0].length;
-        Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
-        int start=intervals[0][0];
-        int end=intervals[0][1];
+
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+
+        int startTime=intervals[0][0];
+        int endTime=intervals[0][1];
         List<List<Integer>> result=new ArrayList<>();
         for(int i=1;i<n;i++){
             int currStart=intervals[i][0];
             int currEnd=intervals[i][1];
-            if(currStart<=end){
-                end=Math.max(end,currEnd);
-            }else{
+            if(currStart<=endTime){
+                endTime=Math.max(currEnd,endTime);
+            }
+            else{
                 List<Integer> temp=new ArrayList<>();
-                temp.add(start);
-                temp.add(end);
-                result.add(temp);
+                temp.add(startTime);
+                temp.add(endTime);
 
-                start=currStart;
-                end=currEnd;
+                result.add(temp);
+                startTime=currStart;
+                endTime=currEnd;
             }
         }
         List<Integer> temp=new ArrayList<>();
-        temp.add(start);
-        temp.add(end);
-
+        temp.add(startTime);
+        temp.add(endTime);
         result.add(temp);
-
-        int matrix[][]=new int[result.size()][2];
-        for(int i=0;i<matrix.length;i++){
-            matrix[i][0]=result.get(i).get(0);
-            matrix[i][1]=result.get(i).get(1);
+        int result1[][]=new int[result.size()][2];
+        for(int i=0;i<result.size();i++){
+            result1[i][0]=result.get(i).get(0);
+            result1[i][1]=result.get(i).get(1);
         }
-        return matrix;
 
+        return result1;
     }
 }
 
-
 // class Solution {
 //     public int[][] merge(int[][] intervals) {
-//         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+//         int n=intervals.length;
+//         int m=intervals[0].length;
+//         Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
 //         int start=intervals[0][0];
 //         int end=intervals[0][1];
-//         List<List<Integer>> temp=new ArrayList<>();
-//         for(int i=1;i<intervals.length;i++){
-//             int s=intervals[i][0];
-//             int e=intervals[i][1];
-//             if(s<=end){
-//                 end=Math.max(end,e);
+//         List<List<Integer>> result=new ArrayList<>();
+//         for(int i=1;i<n;i++){
+//             int currStart=intervals[i][0];
+//             int currEnd=intervals[i][1];
+//             if(currStart<=end){
+//                 end=Math.max(end,currEnd);
 //             }else{
-//                 List<Integer> list=new ArrayList<>();
-//                 list.add(start);
-//                 list.add(end);
-//                 temp.add(list);
-//                 start=s;
-//                 end=e;
+//                 List<Integer> temp=new ArrayList<>();
+//                 temp.add(start);
+//                 temp.add(end);
+//                 result.add(temp);
+
+//                 start=currStart;
+//                 end=currEnd;
 //             }
 //         }
-//         List<Integer> last = new ArrayList<>();
-//         last.add(start);
-//         last.add(end);
-//         temp.add(last);
-//         int result[][]=new int[temp.size()][2];
-//         for(int i=0;i<temp.size();i++){
-//             result[i][0]=temp.get(i).get(0);
-//             result[i][1]=temp.get(i).get(1);
+//         List<Integer> temp=new ArrayList<>();
+//         temp.add(start);
+//         temp.add(end);
+
+//         result.add(temp);
+
+//         int matrix[][]=new int[result.size()][2];
+//         for(int i=0;i<matrix.length;i++){
+//             matrix[i][0]=result.get(i).get(0);
+//             matrix[i][1]=result.get(i).get(1);
 //         }
-//         return result;
+//         return matrix;
+
 //     }
 // }
 
-
-// class Solution {
-//     public int[][] merge(int[][] intervals) {
-//         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-//         int start=intervals[0][0];
-//         int end=intervals[0][1];
-//         List<int[]> list=new ArrayList<>();
-//         for(int i=1;i<intervals.length;i++){
-//             int s=intervals[i][0];
-//             int e=intervals[i][1];
-//             if(s<=end){
-//                 end=Math.max(end,e);
-//             }else{
-//                 list.add(new int[]{start,end});
-//                 start=s;
-//                 end=e;
-//             }
-//         }
-//         list.add(new int[]{start,end});
-//         int result[][]=new int[list.size()][2];
-//         for(int i=0;i<list.size();i++){
-//             result[i]=list.get(i);
-//         }
-//         return result;
-//     }
-// }
