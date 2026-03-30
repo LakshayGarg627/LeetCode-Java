c8de94a8fdd40e602d7aafe10d83731948f845c7
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-03-30 17:22:50 +0530

    Time: 1 ms (63.83%), Space: 43.6 MB (6.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -10,10 +10,10 @@ class Solution {
         }
     }
     Node root=new Node();
-    public void insert(String word){
+    public void insert(String str){
         Node curr=root;
-        for(int i=0;i<word.length();i++){
-            int idx=word.charAt(i)-'a';
+        for(int i=0;i<str.length();i++){
+            int idx=str.charAt(i)-'a';
             if(curr.children[idx]==null){
                 curr.children[idx]=new Node();
                 curr.count++;
@@ -22,11 +22,11 @@ class Solution {
         }
         curr.eow=true;
     }
+
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length==0){
-            return "";
-        }
-        for(int i=0;i<strs.length;i++){
+        int n=strs.length;
+        root=new Node();
+        for(int i=0;i<n;i++){
             insert(strs[i]);
         }
         StringBuilder ans=new StringBuilder("");
@@ -43,6 +43,52 @@ class Solution {
         return ans.toString();
     }
 }
+
+// class Solution {
+//     class Node{
+//         Node children[]=new Node[26];
+//         boolean eow=false;
+//         int count=0;
+//         Node(){
+//             for(int i=0;i<26;i++){
+//                 children[i]=null;
+//             }
+//         }
+//     }
+//     Node root=new Node();
+//     public void insert(String word){
+//         Node curr=root;
+//         for(int i=0;i<word.length();i++){
+//             int idx=word.charAt(i)-'a';
+//             if(curr.children[idx]==null){
+//                 curr.children[idx]=new Node();
+//                 curr.count++;
+//             }
+//             curr=curr.children[idx];
+//         }
+//         curr.eow=true;
+//     }
+//     public String longestCommonPrefix(String[] strs) {
+//         if(strs.length==0){
+//             return "";
+//         }
+//         for(int i=0;i<strs.length;i++){
+//             insert(strs[i]);
+//         }
+//         StringBuilder ans=new StringBuilder("");
+//         Node curr=root;
+//         while(curr.count==1 && curr.eow==false){
+//             for(int i=0;i<26;i++){
+//                 if(curr.children[i]!=null){
+//                     ans.append((char)(i+'a'));
+//                     curr=curr.children[i];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans.toString();
+//     }
+// }
 
 
 // Best
