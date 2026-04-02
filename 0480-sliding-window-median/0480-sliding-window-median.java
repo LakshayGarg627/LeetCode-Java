eb94d167f58dda07c1b1f2633aad47d24a184f08
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-04-03 00:12:56 +0530

    Time: 201 ms (11.07%), Space: 63.3 MB (80.89%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double medianCalc(List<Integer> list,int k){
+    public double medCal(List<Integer> list,int k){
         int n=list.size();
         if(k%2!=0){
             return list.get(n/2)/1.0;
@@ -14,22 +14,57 @@ class Solution {
             temp.add(nums[i]);
         }
         Collections.sort(temp);
-        result[0]=medianCalc(temp,k);
-        for(int i=1;i<=n-k;i++){
+        result[0]=medCal(temp,k);
+        for(int i=1;i<n-k+1;i++){
             int toRemove=nums[i-1];
             int toAdd=nums[i+k-1];
-
             int removeIdx=Collections.binarySearch(temp,toRemove);
             temp.remove(removeIdx);
 
-            int addIdx=Collections.binarySearch(temp,toAdd);
-            if(addIdx<0){
-                addIdx=-addIdx-1;
+            int toAddIdx=Collections.binarySearch(temp,toAdd);
+            if(toAddIdx<0){
+                toAddIdx=-toAddIdx-1;
             }
-            temp.add(addIdx,toAdd);
-            
-            result[i]=medianCalc(temp,k);
+            temp.add(toAddIdx,toAdd);
+
+            result[i]=medCal(temp,k);
         }
         return result;
     }
 }
+
+// class Solution {
+//     public double medianCalc(List<Integer> list,int k){
+//         int n=list.size();
+//         if(k%2!=0){
+//             return list.get(n/2)/1.0;
+//         }
+//         return ((long)list.get((n-1)/2)+list.get(n/2))/2.0;
+//     }
+//     public double[] medianSlidingWindow(int[] nums, int k) {
+//         int n=nums.length;
+//         double result[]=new double[n-k+1];
+//         List<Integer> temp=new ArrayList<>();
+//         for(int i=0;i<k;i++){
+//             temp.add(nums[i]);
+//         }
+//         Collections.sort(temp);
+//         result[0]=medianCalc(temp,k);
+//         for(int i=1;i<=n-k;i++){
+//             int toRemove=nums[i-1];
+//             int toAdd=nums[i+k-1];
+
+//             int removeIdx=Collections.binarySearch(temp,toRemove);
+//             temp.remove(removeIdx);
+
+//             int addIdx=Collections.binarySearch(temp,toAdd);
+//             if(addIdx<0){
+//                 addIdx=-addIdx-1;
+//             }
+//             temp.add(addIdx,toAdd);
+            
+//             result[i]=medianCalc(temp,k);
+//         }
+//         return result;
+//     }
+// }
