e397729000cdd7869576a00b24c1f8143f571f1f
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-04-03 17:11:41 +0530

    Time: 0 ms (100%), Space: 46.5 MB (90.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -22,7 +22,8 @@ public class Solution {
                 break;
             }
         }
-        if(!cycle){
+
+        if(cycle==false){
             return null;
         }
         slow=head;
@@ -34,5 +35,48 @@ public class Solution {
             }
         }
         return slow;
+
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         if(head==null || head.next==null){
+//             return null;
+//         }
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         boolean cycle=false;
+//         while(fast!=null && fast.next!=null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+//             if(slow==fast){
+//                 cycle=true;
+//                 break;
+//             }
+//         }
+//         if(!cycle){
+//             return null;
+//         }
+//         slow=head;
+//         while(slow!=fast){
+//             slow=slow.next;
+//             fast=fast.next;
+//             if(slow==fast){
+//                 break;
+//             }
+//         }
+//         return slow;
+//     }
+// }
