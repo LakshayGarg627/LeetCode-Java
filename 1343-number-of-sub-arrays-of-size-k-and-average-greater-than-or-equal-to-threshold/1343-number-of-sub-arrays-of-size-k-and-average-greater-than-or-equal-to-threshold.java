8b1df73e9b5e03afc45ecefecd6d17110b297f9e
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-05-13 15:27:15 +0530

    Time: 4 ms (28.29%), Space: 71.8 MB (61%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,57 +1,26 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int n=arr.length;
+        int count=0;
         double sum=0;
         for(int i=0;i<k;i++){
             sum=sum+arr[i];
         }
-        int count=0;
-        double avg=sum/k;
-
-        if(avg>=threshold){
+        if((sum/k)>=threshold){
             count++;
         }
-        for(int i=1;i<n-k+1;i++){
-            int include=arr[i+k-1];
-            int exclude=arr[i-1];
+        for(int i=1;i<=n-k;i++){
+            int toRemove=arr[i-1];
+            int toAdd=arr[i+k-1];
 
-            sum=sum-exclude;
-            sum=sum+include;
+            sum=sum-toRemove;
+            sum=sum+toAdd;
 
-            avg=sum/k;
-
-            if(avg>=threshold){
+            if((sum/k)>=threshold){
                 count++;
             }
+            
         }
         return count;
     }
 }
-
-// class Solution {
-//     public int numOfSubarrays(int[] arr, int k, int threshold) {
-//         int n=arr.length;
-//         double sum=0;
-//         int count=0;
-//         for(int i=0;i<k;i++){
-//             sum=sum+arr[i];
-//         }
-//         double avg=sum/k;
-//         if(avg>=threshold){
-//             count++;
-//         }
-//         for(int i=1;i<=n-k;i++){
-//             int exclude=arr[i-1];
-//             int include=arr[i+k-1];
-
-//             sum=sum-exclude;
-//             sum=sum+include;
-
-//             avg=sum/k;
-//             if(avg>=threshold){
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-// }
