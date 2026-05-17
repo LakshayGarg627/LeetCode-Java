c445f5f775404c6caa1b377863e81791bfccb496
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-05-17 22:47:46 +0530

    Time: 2 ms (82.42%), Space: 64 MB (5.48%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,5 +1,24 @@
 class Solution {
+    public boolean helper(int arr[],int idx,int n,Boolean dp[]){
+        if(idx<0 || idx>=n){
+            return false;
+        }
+        if(arr[idx]==0){
+            return true;
+        }
+        if(dp[idx]!=null){
+            return dp[idx];
+        }
+        dp[idx]=false;
+        boolean way1=helper(arr,idx-arr[idx],n,dp);
+        boolean way2=helper(arr,idx+arr[idx],n,dp);
+
+        dp[idx]= way1 || way2;
+        return dp[idx];
+    }
     public boolean canReach(int[] arr, int start) {
-        
+        int n=arr.length;
+        Boolean dp[]=new Boolean[n];
+        return helper(arr,start,n,dp);
     }
 }
