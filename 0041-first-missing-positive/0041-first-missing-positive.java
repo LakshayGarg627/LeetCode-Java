957f1edc303146d363ec3806bc0507b7fb943bcf
LakshayGarg627 <157587677+LakshayGarg627@users.noreply.github.com>
2026-09-24 23:05:40 +0530

    Time: 1 ms (99.94%), Space: 71.3 MB (58.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,3 +1,22 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         for(int i=0;i<nums.length;i++){
+//             while(nums[i]>0 && nums[i]<nums.length && nums[i]!=nums[nums[i]-1]){
+//                 int temp=nums[i];
+//                 nums[i]=nums[temp-1];
+//                 nums[temp-1]=temp;
+//             }
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(i+1!=nums[i]){
+//                 return i+1;
+//             }
+//         }
+//         return nums.length+1;
+
+//     }
+// }
+
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n=nums.length;
@@ -12,9 +31,6 @@ class Solution {
                 i++;
             }
         }
-
-        System.out.println(Arrays.toString(nums));
-
         for(int j=0;j<n;j++){
             if(nums[j]!=j+1){
                 return j+1;
